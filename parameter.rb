36d65abede39255a488052becef862bc6f814d9f
Posmajan2015 <33709274+Posmajan2015@users.noreply.github.com>
2018-04-23 23:02:55 +0700

    Add files via upload

---
+++ parameter.rb
@@ -0,0 +1,14 @@
+def tambahkan(num1, num2)
+  num = num1 + num2
+  puts "angkanya #{num}"
+end
+
+tambahkan(50, 100)
+
+
+def kalikan(num1, num2)
+  num = num1 * num2
+  puts "angkanya #{num}"
+end
+
+kalikan(10, 20)
